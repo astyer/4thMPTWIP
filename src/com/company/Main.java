d9b9b9ebc7cdb45b3e0f4524bd52796d6d9ac18f
astyer <88atti@gmail.com>
2017-04-20 10:31:54 -0400

    in progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,27 +19,59 @@ public class Main {
         //for(int i = 0; i<29; i++)
         //{
             int x = 1;
-            String [] stuff = data.get(x).split("\t");
             //while(stuff[0].equals(i + ""))
             //{
+                String [] stuff = data.get(x).split("\t");
                 String [] q1 = stuff[1].split("\\+");
                 double [] q1points = new double[q1.length-1];
                 for(int p = 1; p < q1.length; p++)
                 {
                     String [] q1parts = q1[p].split(" ");
-                    String num = q1parts[p];
-                    if(num.equals(".5"))
-                    {
-                        num = "0.5";
-                    }
+                    String num = q1parts[1];
                     q1points[p-1] = Double.parseDouble(num);
                 }
                 double q1score = 0;
                 for(int d = 0; d < q1points.length; d++)
                 {
-                    q1score+= q1points[d];
+                    q1score += q1points[d];
+                }
+                if(stuff.length-1 == 2)
+                {
+                    double q1synoff = Integer.parseInt(stuff[2]) * 0.25;
+                    q1score -= q1synoff;
+                }
+
+                if(q1score < 0)
+                {
+                    q1score = 0;
                 }
                 System.out.println(q1score);
+
+                int q2start = 3;
+                if(!(stuff[3].substring(0,1).equals('"')))
+                {
+                    q2start = 4;
+                }
+                String [] q2 = stuff[q2start].split("\\+");
+                double [] q2points = new double[q2.length-1];
+                for(int p = 1; p < q2.length; p++)
+                {
+                    String [] q2parts = q2[p].split(" ");
+                    String num = q2parts[1];
+                    q2points[p-1] = Double.parseDouble(num);
+                }
+                double q2score = 0;
+                for(int d = 0; d < q2points.length; d++)
+                {
+                    q2score += q2points[d];
+                }
+                double q2synoff = Integer.parseInt(stuff[q2start+1]) * 0.25;
+                q2score -= q2synoff;
+                if(q2score < 0)
+                {
+                    q2score = 0;
+                }
+                System.out.println(q2score);
                 //x++;
 
             //}
