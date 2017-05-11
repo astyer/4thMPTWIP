ee8c0c9af226603ec79ef8b3d09707f24daf3ea1
astyer <88atti@gmail.com>
2017-05-10 21:58:21 -0400

    inprog

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,12 +16,17 @@ public class Main {
         }
         sf.close();
 
-        //for(int i = 0; i<29; i++)
-        //{
-            int x = 17;
-            //while(stuff[0].equals(i + ""))
-            //{
-                String [] stuff = data.get(x).split("\t");
+        double [] q1scoreavg = new double[28];
+        double [] q2scoreavg = new double[28];
+
+        int x = 1;
+        for(int i = 1; i < 29; i++)
+        {
+            ArrayList<Double> q1scores = new ArrayList();
+            ArrayList<Double> q2scores = new ArrayList();
+            while(data.get(x).substring(0,1).equals(i + "") || data.get(x).substring(0,2).equals(i + ""))
+            {
+                String [] stuff = data.get(x).split("\t|\\s+\\s+\\s");
                 ArrayList<String> stuffs = new ArrayList();
                 for(String a: stuff)
                 {
@@ -36,7 +41,7 @@ public class Main {
                 }*/
                 for(int f = 0; f < stuffs.size(); f++)
                 {
-                    if(stuffs.get(f).length() > 2)
+                    if(stuffs.get(f).length() > 3)
                     {
                         if(!(stuffs.get(f).substring(1,2).equals("+")))
                         {
@@ -54,15 +59,26 @@ public class Main {
                 }*/
                 for(int f = 0; f < stuffs.size(); f++)
                 {
-                    if(stuffs.get(f).substring(0,1).equals("\"") || stuffs.get(f).substring(0,1).equals("+"))
+                    if(stuffs.get(f).substring(0,1).equals("+"))
                     {
-                        if(stuffs.get(f).substring(0,2).equals("\"+") || stuffs.get(f).substring(0,1).equals("+"))
+                        stuffs.set(f, "\"" + stuffs.get(f));
+                    }
+                }
+                /*for(String b: stuffs)
+                {
+                    System.out.println(b);
+                }*/
+                for(int f = 0; f < stuffs.size(); f++)
+                {
+                    if(stuffs.get(f).substring(0,1).equals("\""))
+                    {
+                        if(stuffs.get(f).substring(1,2).equals("+"))
                         {
                             for(int s = 1; s < stuffs.size()-f; s++)
                             {
-                                if(stuffs.get(f+s).substring(0,1).equals("\"") || stuffs.get(f).substring(0,1).equals("+"))
+                                if(stuffs.get(f+s).substring(0,1).equals("\"") && !(stuffs.get(f+s).substring(0,1).equals("0")))
                                 {
-                                    if(stuffs.get(f+s).substring(0,2).equals("\"+") || stuffs.get(f).substring(0,1).equals("+"))
+                                    if(stuffs.get(f+s).substring(1,2).equals("+"))
                                     {
                                         stuffs.set(f, stuffs.get(f).substring(0,stuffs.get(f).length()-1) + ", " + stuffs.remove(f+s).substring(1));
                                         s--;
@@ -76,10 +92,10 @@ public class Main {
                         }
                     }
                 }
-                for(String b: stuffs)
+                /*for(String b: stuffs)
                 {
                     System.out.println(b);
-                }
+                }*/
                 String [] q1 = stuffs.get(1).split("\\+");
                 double [] q1points = new double[q1.length-1];
                 for(int p = 1; p < q1.length; p++)
@@ -95,14 +111,14 @@ public class Main {
                 }
                 if(!(stuffs.get(2).substring(0,1).equals("\"")))
                 {
-                    double q1synoff = Integer.parseInt(stuffs.get(2)) * 0.25;
+                    double q1synoff = Double.parseDouble(stuffs.get(2)) * 0.25;
                     q1score -= q1synoff;
                 }
                 if(q1score < 0)
                 {
                     q1score = 0;
                 }
-                System.out.println(q1score);
+                q1scores.add(q1score);
 
                 int q2start = 3;
                 if(stuffs.get(2).substring(0,1).equals("\""))
@@ -125,18 +141,66 @@ public class Main {
                 double q2synoff = 0;
                 if(stuffs.size() == q2start+2)
                 {
-                    q2synoff = Integer.parseInt(stuffs.get(q2start+1)) * 0.25;
+                    q2synoff = Double.parseDouble(stuffs.get(q2start+1)) * 0.25;
                 }
                 q2score -= q2synoff;
                 if(q2score < 0)
                 {
                     q2score = 0;
                 }
-                System.out.println(q2score);
-                //x++;
+                q2scores.add(q2score);
 
-            //}
-        //}
+                x++;
+            }
+            double q1avg = 0;
+            for(double f: q1scores)
+            {
+                q1avg += f;
+            }
+            q1avg /= q1scores.size();
+            q1scoreavg[i-1] = q1avg;
 
+            double q2avg = 0;
+            for(double f: q2scores)
+            {
+                q2avg += f;
+            }
+            q2avg /= q2scores.size();
+            q2scoreavg[i-1] = q2avg;
+        }
+
+        Scanner sf2 = new Scanner(new File("named names.txt"));
+
+        ArrayList<String> data2 = new ArrayList();
+        while(sf2.hasNextLine())
+        {
+            data2.add(sf2.nextLine());
+        }
+        sf2.close();
+
+        ArrayList<String> names = new ArrayList();
+        ArrayList<String> secnums = new ArrayList();
+        for(String a: data2)
+        {
+            String [] stuff = a.split("\t");
+            names.add(stuff[0]);
+            secnums.add(stuff[1]);
+        }
+
+        System.out.println("Name    Secret Number   Avg Total Grade FR Q1 Grade FR Q2 Grade");
+
+        for(int i = 0; i < q1scoreavg.length; i++)
+        {
+            /*String q1 = q1scoreavg[i] + "";
+            if(q1.length() < 7)
+            {
+                q1 += "0";
+                q1scoreavg[i] = Double.parseDouble(q1);
+            }*/
+            double avgtotal = q1scoreavg[i] + q2scoreavg[i];
+            int n = i+1;
+            System.out.print("\t\t" + n + "\t\t\t\t" + avgtotal + " / 19" + "\t\t" + q1scoreavg[i] + "\t\t" + q2scoreavg[i]);
+            System.out.println();
+        }
     }
 }
